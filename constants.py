a77c8f2956e716b0175cf585f43bde8c3ccc5852
oleksandra.sherstiuk <as.sherstiuk@gmail.com>
2023-11-13 17:46:53 +0100

    math exercises script

--- constants.py
+++
@@ -1,8 +0,0 @@
-class ArithmeticOperations:
-    """
-    class to store math (arithmetic) operations names
-    """
-    SUBTRACTION = 'subtraction'
-    ADDITION = 'addition'
-    DIVISION = 'division'
-    MULTIPLICATION = 'multiplication'
